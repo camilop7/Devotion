680ef6794f4f9915a9c9c5aa990f546082711bbd
camilo palacios <camilop7@icloud.com>
2023-10-20 22:42:40 +0100

    grid cards, shared nav, responsive grid, card product

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'roasters', to: 'roasters#index'
+  get 'shops', to: 'shops#index'
+  get 'farmers', to: 'farmers#index'
+
+
+
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
