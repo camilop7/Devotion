5474c375fc8a3448cf72bea5495567734e85d23b
camilo palacios <camilop7@icloud.com>
2023-10-21 22:13:10 +0100

    cards layouts play1

--- app/assets/javascripts/carousel.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  var carouselElement = document.getElementById('carouselExampleCaptions');
-  var carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 5000, // Set the interval (in milliseconds) between slides
-    pause: 'hover', // Pause the carousel on hover
-    wrap: true // Allow continuous looping of slides
-  });
-});
