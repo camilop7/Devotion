426189f7283a32b86de7f3bd75e50e48b4b031a8
camilo palacios <camilop7@icloud.com>
2023-10-29 20:24:37 +0000

    unstaged heroku duplicates deleted

--- config/webpack/environment 2.js
+++
@@ -1,7 +0,0 @@
-const { environment } = require('@rails/webpacker')
-
-const { VueLoaderPlugin } = require('vue-loader')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-
-module.exports = environment
